4008bd95cc49669c6ed9c76d50b9224d1d276784
khal1lulloh <4476362@gmail.com>
2022-05-26 19:12:52 +0500

    bu nmadirde

--- Kodlar_jamlammasi/kod_app/urls.py
+++ Kodlar_jamlammasi/kod_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView,PageView,AboutPageView,WorksPageView,JournalPageView,ContactsPageView
 
 urlpatterns = [
 	path('',HomePageView.as_view(),name = 'index'),
+	path('home/',PageView.as_view(),name = 'home'),
+	path('about/',AboutPageView.as_view(),name='about'),
+	path('works/',WorksPageView.as_view(),name='works'),
+	path('journal/',JournalPageView.as_view(),name='journal'),
+	path('contacts/',ContactsPageView.as_view(),name='contacts'),
 	]
--- Kodlar_jamlammasi/kod_app/views.py
+++ Kodlar_jamlammasi/kod_app/views.py
@@ -3,3 +3,13 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
 	template_name = 'index.html' 
+class PageView(TemplateView):
+	template_name='home.html'
+class AboutPageView(TemplateView):
+	template_name='about.html'
+class WorksPageView(TemplateView):
+	template_name='works.html'
+class JournalPageView(TemplateView):
+	template_name='journal.html'
+class ContactsPageView(TemplateView):
+	template_name='contacts.html'
